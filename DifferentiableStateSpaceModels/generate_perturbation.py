309533339d672be27102f792b10159bb1e46fff2
zhenqihu <zhenqi_hu@outlook.com>
2023-12-27 20:55:53 +0800

    generate_perturbation.py (12.27.v02)

--- DifferentiableStateSpaceModels/generate_perturbation.py
+++ DifferentiableStateSpaceModels/generate_perturbation.py
@@ -8,17 +8,13 @@ import numpy as np
 from DifferentiableStateSpaceModels.utils import fill_zeros, maybe_call_function
 from DifferentiableStateSpaceModels.symbolic_utils import order_vector_by_symbols
 from DifferentiableStateSpaceModels.types import *
-
-# Utility to call the cache's organized by symbol
-def fill_array_by_symbol_dispatch(f, c, symbols, *args):
-    for i, sym in enumerate(symbols):
-        f(c[i], sym, *args)
+from scipy.optimize import fsolve
 
 
-def create_or_zero_cache(m, cache, Order, p_d, zero_cache):
+def create_or_zero_cache(m, cache, order, p_d, zero_cache):
     if cache is None:
         # create it if not supplied
-        return SolverCache.from_model(m, Order, p_d)
+        return SolverCache.from_model(m, order, p_d)
     else:
         # otherwise conditionally zero it and return the argument
         if zero_cache:
@@ -43,7 +39,7 @@ def generate_perturbation(m, p_d, p_f, order, cache=None, zero_cache=False, sett
     c = create_or_zero_cache(m, cache, order, p_d, zero_cache)  # SolverCache object
     c.p_d_symbols = list(p_d.keys())
 
-    p = p_d if p_f is None else order_vector_by_symbols({**p_d, **p_f}, m.mod.m.p_symbols)
+    p = list(p_d.values()) if p_f is None else order_vector_by_symbols({**p_d, **p_f}, m.mod.m.p_symbols)
     if order == 1:
         # First order solutions
         # solver type provided to all callbacks
@@ -90,26 +86,106 @@ def calculate_steady_state(m, c, settings, p):
 
     if settings.print_level > 2:
         print("Calculating steady state")
+    try:
+        if (m.mod.m.y_bar is not None) and (m.mod.m.x_bar is not None):  # use closed form if possible
+            c.y = m.mod.m.y_bar(p)
+            c.x = m.mod.m.x_bar(p)
+            for i, sym in enumerate(c.p_d_symbols):
+                f_y = getattr(m.mod.m, 'y_bar_p' + '_' + sym)
+                if f_y is not None:
+                    c.y_p[i] = f_y(p)
 
-    #try:
-    #    if m.mod.m.y_bar is not None and m.mod.m.x_bar is not None:
-    #        # use the user-supplied steady state
-    #        c.y_bar = m.mod.m.y_bar(p)
-    #        c.x_bar = m.mod.m.x_bar(p)
-    return None
+                f_x = getattr(m.mod.m, 'x_bar_p' + '_' + sym)
+                if f_x is not None:
+                    c.x_p[i] = f_x(p)
+        elif m.mod.m.steady_state is not None:  # use user-provided calculation otherwise
+            c.y, c.x = m.mod.m.steady_state(p)
+        else:  # fallback is to solve system of equations from user-provided initial condition
+            # TODO: solve steady state
+            y_0 = np.zeros(n_y)
+            x_0 = np.zeros(n_x)
+            if m.mod.m.y_bar_iv is not None:
+                y_0 = m.mod.m.y_bar_iv(p)
+            if m.mod.m.x_bar_iv is not None:
+                x_0 = m.mod.m.x_bar_iv(p)
+            w_0 = np.concatenate((y_0, x_0))
+    except Exception as e:
+        if settings.rethrow_exceptions:
+            raise e
+        elif isinstance(e, np.linalg.LinAlgError):  # equivalent to LAPACKException in Julia
+            if settings.print_level > 0:
+                print(e)
+            return 'LAPACK_Error'
+        elif isinstance(e, np.linalg.LinAlgError):  # equivalent to PosDefException in Julia
+            if settings.print_level > 0:
+                print(e)
+            return 'POSDEF_EXCEPTION'
+        elif isinstance(e, ValueError):  # equivalent to DomainError in Julia
+            if settings.print_level > 0:
+                print(e)
+            return 'Evaluation_Error'  # function evaluation error
+        else:
+            if settings.print_level > 0:
+                print(e)
+            return 'FAILURE'  # generic failure
+    return "Success"
 
 
 def evaluate_first_order_functions(m, c, settings, p):
-    return None
+    if settings.print_level > 2:
+        print("Evaluating first-order functions into cache")
+    try:
+        y, x = c.y, c.x  # Precondition: valid (y, x) steady states
+        c.H_yp = m.mod.m.H_yp(y, x, p)
+        c.H_y = m.mod.m.H_y(y, x, p)
+        c.H_xp = m.mod.m.H_xp(y, x, p)
+        c.H_x = m.mod.m.H_x(y, x, p)
+        c.Γ = m.mod.m.Γ(p)
+        c.Ω = maybe_call_function(m.mod.m.Ω, p)
+        if len(c.p_d_symbols) > 0:
+            c.Ψ = m.mod.m.Ψ(y, x, p)
+    except Exception as e:
+        if settings.rethrow_exceptions:
+            raise e
+        elif isinstance(e, ValueError):  # equivalent to DomainError in Julia
+            if settings.print_level > 0:
+                print(e)
+            return 'Evaluation_Error'  # function evaluation error
+        else:
+            if settings.print_level > 0:
+                print(e)
+            return 'FAILURE'  # generic failure
+    return "Success"
 
 
 def evaluate_second_order_functions(m, c, settings, p):
-    return None
+    if settings.print_level > 2:
+        print("Evaluating second-order functions into cache")
+    try:
+        y, x = c.y, c.x  # Precondition: valid (y, x) steady states
+        if len(c.p_d_symbols) == 0:
+            c.Ψ = m.mod.m.Ψ(y, x, p)  # would have been called otherwise in first_order_functions
+        c.Ψ_yp = m.mod.m.Ψ_yp(y, x, p)
+        c.Ψ_y = m.mod.m.Ψ_y(y, x, p)
+        c.Ψ_xp = m.mod.m.Ψ_xp(y, x, p)
+        c.Ψ_x = m.mod.m.Ψ_x(y, x, p)
+    except Exception as e:
+        if settings.rethrow_exceptions:
+            raise e
+        elif isinstance(e, ValueError):  # equivalent to DomainError in Julia
+            if settings.print_level > 0:
+                print(e)
+            return 'Evaluation_Error'  # function evaluation error
+        else:
+            if settings.print_level > 0:
+                print(e)
+            return 'FAILURE'  # generic failure
+    return "Success"  # no failing code-paths yet.
 
 
 def solve_first_order(m, c, settings):
-    return None
+    return "Success"
 
 
 def solve_second_order(m, c, settings):
-    return None
+    return "Success"
